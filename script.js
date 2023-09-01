e4f4e0a3aafd7df1aa182c85ec0eb30e6816e7e7
MOHAMMAD NOUSHAD <116102583+noushadkas@users.noreply.github.com>
2023-09-01 17:50:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const nav= document.getElementById("browser-info")
+nav.innerText=`You are using ${window.navigator.appName} version ${window.navigator.appVersion}`
